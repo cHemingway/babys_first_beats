57500c5a5ca1db0ca5572147812e2eab22610a97
Chris Hemingway <chris.hemingway@gmail.com>
2025-04-25 21:15:03 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Baby's First Beats
+
+Sample box for a very small child, to introduce them to DnB/Garage/Jungle etc
+
+### Hardware
+- [Adafruit RP2040 Prop-Maker Feather](https://learn.adafruit.com/adafruit-rp2040-prop-maker-feather)
+- [Mini speaker 4R 3W](https://shop.pimoroni.com/products/mini-speaker-4-3w)
+- [2200mAh LiPo battery pack](https://shop.pimoroni.com/products/lithium-ion-battery-pack?variant=23417820359)
+- [Colourful Arcade Buttons](https://shop.pimoroni.com/products/colourful-arcade-buttons?variant=451785353)
+- 3D Printed Case, see /cad
+
+### Code
+- Runs on CircuitPython 9.0
+- Hardware tesst project in `code/hw_test` 
---
+++ code/hw_test/code.py
@@ -0,0 +1,88 @@
+import alarm
+import audiocore
+import audiobusio
+import audiomixer
+import board
+import busio
+import time
+
+from digitalio import DigitalInOut, Direction, Pull
+
+import neopixel
+import adafruit_lis3dh
+
+print("Start")
+
+# Connect button
+button = DigitalInOut(board.EXTERNAL_BUTTON)
+
+# Turn on neopixel
+pixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
+pixel.brightness = 0.1
+
+pixel.fill((0xFF,0xA5,0))
+
+# Speak to accelerometer
+i2c = board.I2C()  # uses board.SCL and board.SDA
+accel_int1 = DigitalInOut(board.ACCELEROMETER_INTERRUPT)
+accel = adafruit_lis3dh.LIS3DH_I2C(i2c, int1=accel_int1)
+accel.range = adafruit_lis3dh.RANGE_4_G
+
+
+# Set tap detection to double taps.  The first parameter is a value:
+#  - 0 = Disable tap detection.
+#  - 1 = Detect single taps.
+#  - 2 = Detect double taps.
+
+# The second parameter is the threshold and a higher value means less sensitive
+# tap detection.  Note the threshold should be set based on the range above:
+#  - 2G = 40-80 threshold
+#  - 4G = 20-40 threshold
+#  - 8G = 10-20 threshold
+#  - 16G = 5-10 threshold
+accel.set_tap(1, 100)
+
+# enable external power pin
+# provides power to the external components
+external_power = DigitalInOut(board.EXTERNAL_POWER)
+external_power.direction = Direction.OUTPUT
+external_power.value = True
+
+print("Power On")
+
+# i2s playback
+wave_file = open("StreetChicken.wav", "rb")
+wave = audiocore.WaveFile(wave_file)
+audio = audiobusio.I2SOut(board.I2S_BIT_CLOCK, board.I2S_WORD_SELECT, board.I2S_DATA)
+mixer = audiomixer.Mixer(voice_count=1, sample_rate=22050, channel_count=1,
+                         bits_per_sample=16, samples_signed=True)
+audio.play(mixer)
+mixer.voice[0].level = 0.1
+mixer.voice[0].play(wave, loop=False)
+print("Playing")
+
+#Keep LED on if playing
+pixel.fill((0x00,0x00,0xFF))
+
+
+while mixer.voice[0].playing:
+    time.sleep(0.1)
+    if accel.tapped:
+        pixel.fill((0xFF,0xFF,0xFF))
+    else:
+        pixel.fill((0x00,0x00,0xFF))
+
+
+pixel.fill((0x00, 0xFF, 0x00))
+print("Done")
+
+
+# Go to sleep until button is pressed
+pixel.fill((0,0,0))
+external_power.value = False
+accel_int1.deinit() # release
+accel_alarm = alarm.pin.PinAlarm(board.ACCELEROMETER_INTERRUPT, value=True)
+button.deinit()
+button_alarm = alarm.pin.PinAlarm(board.EXTERNAL_BUTTON, value=False, pull=True)
+print("Going to sleep")
+alarm.exit_and_deep_sleep_until_alarms(accel_alarm, button_alarm)
