e2ed50ee8d9ab6af1f8d1019c82e3f11161bdeda
Chris Hemingway <chris.hemingway@gmail.com>
2025-05-11 19:44:43 +0100

    Initial app implementation on final keypads

--- README.md
+++ README.md
@@ -11,8 +11,16 @@ Sample box for a very small child, to introduce them to DnB/Garage/Jungle etc
 
 ### Code
 - Runs on CircuitPython 9.0
-- Hardware tesst project in `code/hw_test` 
+- Hardware test project in `code/hw_test` 
 
 ### Samples
 - https://www.reddit.com/r/Drumkits/comments/h9o71o/remeber_the_dj_button_on_the_keyboards_in_school/
 - https://www.reddit.com/r/Drumkits/comments/lug5zw/0gjungle_warfare_1_2_3_official_90s_junglednb/
+
+### TODO
+- Add various modes
+    - Press key 6 to cycle
+    - Neopixel LED should indicate mode
+- Add SD card support to get more samples
+- Turn self on/off by driving EN diode-or'd with button
+- Read battery with external resistor divider
--- code/app/code.py
+++ code/app/code.py
@@ -1,48 +1,45 @@
-import alarm
-import alarm.pin
 import audiocore
 import audiobusio
 import audiomixer
 import board
 import time
+import keypad
 
-from digitalio import DigitalInOut, Direction, Pull
+from digitalio import DigitalInOut, Direction
+import analogio
 
 import neopixel
-from adafruit_debouncer import Button
 
 print("Start")
 start_time = time.monotonic_ns()
 
+# Setup keypad
+keypad = keypad.KeyMatrix(
+    row_pins = [board.D5, board.D6, board.D9],
+    column_pins = [board.D10, board.D11, board.D12],
+)
+
+
+# Volume control
+volume_dial = analogio.AnalogIn(board.A0)
+def get_volume():
+    # Convert the raw value to a volume level between 0 and 1
+    scale = volume_dial.value / 65535
+    # Scale logarithmically to get a more natural volume curve
+    volume = scale ** 1.4
+    return volume * 0.25  # Scale to 0.5 to limit the volume
+
+
 # Table of button pins and audio files
-PIN_DEFINITIONS = [
-    {"pin": board.EXTERNAL_BUTTON, "type":"loop", "filename": "Fat Upright 2 (JW3)_mono22k.wav"},
-    {"pin": board.D5, "type":"sample", "filename": "Kick 06 (JW2)_mono22k.wav"},
-    {"pin": board.D6, "type":"sample", "filename": "Snare 39 (JW2)_mono22k.wav"},
-    {"pin": board.D9, "type":"sample", "filename": "Snare 09 (JW2)_mono22k.wav"}
+KEY_DEFINITIONS = [
+    {"key": 7, "type":"loop", "filename": "Diesel Not Petrol [-JqV1uJQaLU]_mono22k.wav"},
+    {"key": 0, "type":"sample", "filename": "Worries Soundboy [2025-04-27 210147]_mono22k.wav"},
+    {"key": 1, "type":"sample", "filename": "Killa Vox [2025-04-27 210139]_mono22k.wav"},
+    {"key": 2, "type":"sample", "filename": "Ruling Bad Boy Tune [2025-04-27 210146]_mono22k.wav"},
+    {"key": 3, "type":"sample", "filename": "Champion Badman [2025-04-27 210142]_mono22k.wav"},
+    {"key": 4, "type":"sample", "filename": "bleep [2025-04-27 210136]_mono22k.wav"},
+    {"key": 5, "type":"sample", "filename": "Air Horn Sound Effect [IpyingiCwV8]_mono22k.wav"}
 ]
-
-
-def setup_buttons():
-    ''' Set up the buttons with pull-up resistors and debouncing '''
-    for pin_def in PIN_DEFINITIONS:
-        digital= DigitalInOut(pin_def["pin"])
-        digital.direction = Direction.INPUT
-        digital.pull = Pull.UP
-        pin_def["digital"] = digital
-        pin_def["button"] = Button(digital, interval=0.01)
-setup_buttons()
-
-def setup_button_alarms() -> list:
-    ''' Set up alarms for each button pin so that they can wake the board from sleep '''
-    alarms = []
-    for pin_def in PIN_DEFINITIONS:
-        # Disconnect the pin from the DigitalInOut object
-        pin_def["digital"].deinit()
-        # Set up the pin as a PinAlarm
-        pin_alarm = alarm.pin.PinAlarm(pin_def["pin"], value=False, pull=True)
-        alarms.append(pin_alarm)
-    return alarms
 
 # Setup neopixel
 pixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
@@ -53,8 +50,9 @@ pixel.fill((0xFF,0x00,0x00))
 audio = audiobusio.I2SOut(board.I2S_BIT_CLOCK, board.I2S_WORD_SELECT, board.I2S_DATA)
 mixer = audiomixer.Mixer(voice_count=2, sample_rate=22050, channel_count=1,
                          bits_per_sample=16, samples_signed=True)
-mixer.voice[0].level = 0.25   # Sample
-mixer.voice[1].level = 0.5    # Loop
+volume = get_volume()
+mixer.voice[0].level = volume   # Sample
+mixer.voice[1].level = volume*0.5    # Loop
 audio.play(mixer)
 
 # External power pin
@@ -63,7 +61,7 @@ external_power.direction = Direction.OUTPUT
 external_power.value = True
 end_time = time.monotonic_ns()
 print(f"Setup time: {(end_time - start_time)*1E-6:.3f} ms")
-print(f"Entering main loop")
+print("Entering main loop")
 
 # Setup done
 pixel.fill((0x00,0x00,0x00))
@@ -84,63 +82,54 @@ def currently_playing():
 
 while True:
     # Check for button presses
-    for pin_def in PIN_DEFINITIONS:
-        pin_def["button"].update()
-        if pin_def["button"].pressed:
-            start_time = time.monotonic_ns()
-
+    if event := keypad.events.get():
+        if event.pressed:
             # Check if the button is a loop or sample
-            if pin_def["type"] == "loop":
-                # If the button is a loop, stop any currently playing loop
-                if mixer.voice[1].playing:
-                    mixer.voice[1].stop()
-                # If we were previously playing this loop, then don't play it again
-                if current_loop and current_loop["filename"] == pin_def["filename"]:
-                    print(f"Already playing {pin_def['filename']}")
-                    current_loop = None
-                    continue
-                # Load the new sound
-                wave_file = open(pin_def["filename"], "rb")
-                pin_def["wave"] = audiocore.WaveFile(wave_file)
-                # Start the sound
-                mixer.voice[1].play(pin_def["wave"], loop=True)
-                current_loop = pin_def
-                last_played_time = time.monotonic()
-            elif pin_def["type"] == "sample":
-                # If the button is a sample, stop any currently playing sound
-                if mixer.voice[0].playing:
-                    mixer.voice[0].stop()
-                # Load the new sound
-                wave_file = open(pin_def["filename"], "rb")
-                pin_def["wave"] = audiocore.WaveFile(wave_file)
-                # Start the sound
-                mixer.voice[0].play(pin_def["wave"])
-                current_sample = pin_def
-                last_played_time = time.monotonic()
-                # Turn on the neopixel
-                pixel.fill((0x00, 0xFF, 0x00))
-            else:
-                print(f"Unknown type {pin_def['type']} for {pin_def['filename']}")
-                continue
+            for pin_def in KEY_DEFINITIONS:
+                if event.key_number == pin_def["key"]:
+                    start_time = time.monotonic_ns()
+                    # If the button is a loop, stop any currently playing loop
+                    if pin_def["type"] == "loop":
+                        if mixer.voice[1].playing:
+                            mixer.voice[1].stop()
+                        # If we were previously playing this loop, then don't play it again
+                        if current_loop and current_loop["filename"] == pin_def["filename"]:
+                            print(f"Already playing {pin_def['filename']}")
+                            current_loop = None
+                            continue
+                        # Load the new sound
+                        wave_file = open(pin_def["filename"], "rb")
+                        pin_def["wave"] = audiocore.WaveFile(wave_file)
+                        # Start the sound
+                        mixer.voice[1].play(pin_def["wave"], loop=True)
+                        current_loop = pin_def
+                        last_played_time = time.monotonic()
+                    elif pin_def["type"] == "sample":
+                        # If the button is a sample, stop any currently playing sound
+                        if mixer.voice[0].playing:
+                            mixer.voice[0].stop()
+                        # Load the new sound
+                        wave_file = open(pin_def["filename"], "rb")
+                        pin_def["wave"] = audiocore.WaveFile(wave_file)
+                        # Start the sound
+                        mixer.voice[0].play(pin_def["wave"])
+                        current_sample = pin_def
+                        last_played_time = time.monotonic()
+                        # Turn on the neopixel
+                        pixel.fill((0x00, 0xFF, 0x00))
+                    else:
+                        print(f"Unknown type {pin_def['type']} for {pin_def['filename']}")
+                    end_time = time.monotonic_ns()
+                    print(f"Latency: {(end_time - start_time)*1E-6:.3f} ms")
 
-            end_time = time.monotonic_ns()
-            print(f"Latency: {(end_time - start_time)*1E-6:.3f} ms")
-
+    # Check if the volume dial has changed
+    volume = get_volume()
+    if volume != mixer.voice[0].level:
+        mixer.voice[0].level = volume * 1
+        mixer.voice[1].level = volume * 0.5
     
     # Wait for the sound to finish
     if current_sample and not mixer.voice[0].playing:
         # Turn off the neopixel
         pixel.fill((0x00, 0x00, 0x00))
         current_sample = None
-
-    # If its been 10 seconds since the last sound was played, go to sleep
-    if not currently_playing() and time.monotonic() - last_played_time > 10:
-        alarms = setup_button_alarms()
-        print("Going to sleep")
-        # Turn off external power pin which deactivates the audio amplifier
-        external_power.value = False
-        # Go to sleep
-        alarm.exit_and_deep_sleep_until_alarms(*alarms)
-        
-
-    time.sleep(0.005)
