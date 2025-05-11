8a73c8744735cde52dbd64b267000dd5912eb891
Chris Hemingway <chris.hemingway@gmail.com>
2025-05-11 18:46:12 +0100

    Change hardware test to match new hardware

--- code/hw_test/code.py
+++ code/hw_test/code.py
@@ -13,14 +13,16 @@ import adafruit_lis3dh
 
 print("Start")
 
-# Connect button
-button = DigitalInOut(board.EXTERNAL_BUTTON)
-
 # Turn on neopixel
 pixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
 pixel.brightness = 0.1
 
 pixel.fill((0xFF,0xA5,0))
+
+# Turn on external neopixel
+external_pixel = neopixel.NeoPixel(board.EXTERNAL_NEOPIXEL, 1)
+external_pixel.brightness = 0.1
+external_pixel.fill((0xFF,0xA5,0))
 
 # Speak to accelerometer
 i2c = board.I2C()  # uses board.SCL and board.SDA
@@ -51,7 +53,7 @@ external_power.value = True
 print("Power On")
 
 # i2s playback
-wave_file = open("StreetChicken.wav", "rb")
+wave_file = open("hypefunk - antonio - drumloop compressed_mono22k.wav", "rb")
 wave = audiocore.WaveFile(wave_file)
 audio = audiobusio.I2SOut(board.I2S_BIT_CLOCK, board.I2S_WORD_SELECT, board.I2S_DATA)
 mixer = audiomixer.Mixer(voice_count=1, sample_rate=22050, channel_count=1,
@@ -62,7 +64,7 @@ mixer.voice[0].play(wave, loop=False)
 print("Playing")
 
 #Keep LED on if playing
-pixel.fill((0x00,0x00,0xFF))
+external_pixel.fill((0x00,0x00,0xFF))
 
 
 while mixer.voice[0].playing:
@@ -73,16 +75,6 @@ while mixer.voice[0].playing:
         pixel.fill((0x00,0x00,0xFF))
 
 
-pixel.fill((0x00, 0xFF, 0x00))
+external_pixel.fill((0x00, 0xFF, 0x00))
 print("Done")
 
-
-# Go to sleep until button is pressed
-pixel.fill((0,0,0))
-external_power.value = False
-accel_int1.deinit() # release
-accel_alarm = alarm.pin.PinAlarm(board.ACCELEROMETER_INTERRUPT, value=True)
-button.deinit()
-button_alarm = alarm.pin.PinAlarm(board.EXTERNAL_BUTTON, value=False, pull=True)
-print("Going to sleep")
-alarm.exit_and_deep_sleep_until_alarms(accel_alarm, button_alarm)
